d8d60464bf5e99ec71c8bfa217e0774e9f405ed6
jonnebonde <jonnebonde@gmail.com>
2022-01-20 08:19:09 +0100

    enter

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// question 1
+
+const instrument = "guitar";
+
+console.log(instrument);
+
+
+// question 2
+
+const button = document.querySelector("button.login");
+
+console.log(button);
+
+
+// question 3
+
+const instruments = [
+    {
+        type: "guitar",
+        colour: "red",
+    },
+    {
+        type: "piano",
+        colour: "black",
+    },
+];
+
+console.table(instruments);
+
+
+// question 4
+
+//var name = "Percival";
+//var age = 13;
+
+const name = "Percival";
+let age = 13;
+
+
+// question 5
+
+//var total = 0;
+//total = total + 10;
+
+const total = 0;
+total = total + 10;
+
+
+// question 6
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+// question 7
+
+const firstName = "Florence";
+
+const introduction = "Hello, my name is " + firstName + ".";
+
+console.log(introduction);
